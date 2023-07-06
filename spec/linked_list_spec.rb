c70a7e7908169df2c687688e11151d410c7276a2
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-06 15:27:53 -0500

    fix spelling error line 10, .new.

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe LinkedList do
   describe "#Iteration1, LinkedList" do 
 
     it 'exists with attributes' do 
-      list = LinkedList.news
+      list = LinkedList.new
     end
   end
 end
