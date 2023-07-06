40b7eebd703d0a059c7e1cedfbb0a7b0bd2bc9a2
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-06 13:53:41 -0500

    add data and next_node attributes to node class, write test for initialize and attributes.

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -2,7 +2,9 @@ require 'rspec'
 require './lib/node'
 
 RSpec.describe Node do 
+
   describe '#Iteration 1' do
+  
     it 'exists with attributes' do 
       node = Node.new("plop")
       expect(node.data).to eq("plop")
