226bdb3b1d31d7bed007994f6fb4d08856410ba4
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-07 09:32:18 -0500

    test logic for append method.

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,16 +1,16 @@
 class LinkedList
 
-  attr_reader :head 
+  attr_reader :head
   def initialize
     @head = nil
   end
 
   def append(data)
-    @head << Node.new(data) 
+    @head = Node.new(data) 
   end
 
   def count
-    
+    require 'pry'; binding.pry
   end
 
   def to_string
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -5,13 +5,11 @@ require './lib/node'
 RSpec.describe LinkedList do 
 
   describe "#Iteration1, LinkedList" do 
-
     it 'exists with attributes' do 
       list = LinkedList.new
 
       expect(list.head).to eq(nil)
       list.append("doop")
-      expect(list).to eq(doop)
       expect(list.head.data).to eq("doop")
       expect(list.head.next_node).to eq(nil)
       expect(list.count).to eq(1)
