5758764e1166059565349aca026149d87300a543
Blake Sergesketter <blakesergesketter@gmail.com>
2023-07-10 21:23:48 -0500

    complete testing for itr 1 interaction pattern, and test all methods itr 1 methods.

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -15,20 +15,21 @@ RSpec.describe LinkedList do
       expect(list.head.next_node).to eq(nil)
       expect(list.count).to eq(1)
       expect(list.to_string).to eq("doop")
-      # require 'pry'; binding.pry
     end
 
-    # it 'adds multiple nodes to lniked list' do
-    #   list = LinkedList.new
+    it 'adds multiple nodes to lniked list' do
+      list = LinkedList.new
 
-    #   expect(list.head).to eq(nil)
-    #   list.append("doop")
-    #   expect(list).to eq(list)
-    #   expect(list.head.data).to eq("doop")
-    #   expect(list.head.next_node).to eq(nil)
-    #   expect(list.count).to eq(1)
-    #   expect(list.to_string).to eq("doop")
-
-    # end
+      expect(list.head).to eq(nil)
+      list.append("doop")
+      expect(list).to eq(list)
+      expect(list.head.data).to eq("doop")
+      expect(list.head.next_node).to eq(nil)
+      list.append("deep")
+      expect(list).to eq(list)
+      expect(list.head.next_node).to eq(list.head.next_node)
+      expect(list.count).to eq(2)
+      expect(list.to_string).to eq("doop deep")
+    end
   end
 end
